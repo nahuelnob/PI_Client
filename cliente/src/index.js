1e3c63dfc845f5cd910a34e179701abfe8804ec8
Nahue <nahue.santini@gmail.com>
2023-09-18 19:39:25 -0300

    Deploy

--- cliente/src/index.js
+++ cliente/src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:3001'
+// axios.defaults.baseURL = 'http://localhost:3001'
+axios.defaults.baseURL = 'https://piserver-production.up.railway.app/'
 
 import {Provider} from 'react-redux'
 import store from './redux/store/store'
